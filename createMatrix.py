fixed calculation of derivative

createMatrix.py
@@ -25,7 +25,7 @@ def generate_outer_matrix(n):
                 A[k][k] += 1 # neumann condition
             elif j == n - 1:
                 A[k][ind(i, j + 1, n)] -= 1
-    return A/float(dx^2)
+    return A/float(dx*dx)
     
 def generate_outer_rhs(n, bc_derivative):
     dx = 1/(n + 1)
@@ -41,7 +41,7 @@ def generate_outer_rhs(n, bc_derivative):
                 rhs[k] += dx*bc_derivative[i]
             elif j == n - 1:
                 rhs[k] += -40
-    return rhs/ float(dx^2)
+    return rhs/ float(dx*dx)
     
 def generate_inner_matrix(n): 
     dx = 1/(n + 1) # dirichlet conditions for the large room 
@@ -51,7 +51,7 @@ def generate_inner_matrix(n):
     for k in range(1, 2*n + 1):
         sup_sub[n*k -1] = 0
     A += np.diag(sup_sub, 1) + np.diag(sup_sub, -1)
-    return A/float(dx^2)
+    return A/float(dx*dx)
 
 def generate_inner_rhs_init(n, gamma_H = 40, gamma_N = 15, gamma_WF = 5):
     '''Creates an initiate b1 (A1 x = b1) which is to be stored and used in
@@ -75,7 +75,7 @@ def generate_inner_rhs_init(n, gamma_H = 40, gamma_N = 15, gamma_WF = 5):
         if k%n == n-1:
             b[k] -= gamma_N
             
-    return b/float(dx^2)
+    return b/float(dx*dx)
     
 def generate_inner_rhs(inner_rhs_initiated, gamma_1, gamma_2):
     '''Updates b1 with the new Dirichlet conditions. OBS: gamma:s are as in 
@@ -88,11 +88,11 @@ def generate_inner_rhs(inner_rhs_initiated, gamma_1, gamma_2):
     
     for k in range(first_loop):
         if k%n == n-1:
-            inner_rhs_initiated[k] -= gamma_2[k//n]/float(dx^2)
+            inner_rhs_initiated[k] -= gamma_2[k//n]/float(dx*dx)
     
     for k in range(first_loop, nelm):
         if k%n == 0:
-            inner_rhs_initiated[k] -= gamma_1[(k-1)//n - n]/float(dx^2)
+            inner_rhs_initiated[k] -= gamma_1[(k-1)//n - n]/float(dx*dx)
             
     return inner_rhs_initiated
 

mpitest.py
@@ -35,13 +35,8 @@ for i in range(n_iter):
     # calculate solution
     if is_outer(rank):
         bc_derivative = np.zeros((n, 1))
-        if rank == 0:
-            for j in range(n):
-                bc_derivative[j] = (sol[(n - 1) + j*n] - bc_rec[j])/float(2*dx)
-        else:
-            for j in range(n):
-                bc_derivative[j] = (sol[j*n] - bc_rec[j])/float(2*dx)
-        #bc_derivative *= 0
+        for j in range(n):
+            bc_derivative[j] = (sol[j*n] - bc_rec[j])/float(2*dx)
         rhs = createMatrix.generate_outer_rhs(n, bc_derivative)
     else:
         rhs = createMatrix.generate_inner_rhs(rhs_init.copy(), bc_rec_from_0, bc_rec_from_2)